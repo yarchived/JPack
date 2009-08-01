c25f66ac2eb281f72802593c1aaf86f431cbbbb6
yleaf <yleaf@1513539b-a478-4f4c-8a8a-170ecc93949f>
2009-08-01 06:30:10 +0000

    toc version update example custom order
    git-svn-id: svn://svn.curseforge.net/wow/jpack/mainline/trunk@62 1513539b-a478-4f4c-8a8a-170ecc93949f

--- JPackConfig.lua
+++ JPackConfig.lua
@@ -6,3 +6,30 @@
 --JPACK_DEPOSIT = {}
 --JPACK_DRAW = {}
 
+
+--@debug@
+-- example
+if JPack.DEV_MOD then
+	JPACK_ORDER = {
+		'炉石',
+		'气阀微粒提取器',
+		'矿工锄',
+		'剥皮小刀',
+		'#鱼竿',
+		'##材料',
+		'#武器',
+		'#护甲',
+		'#配方',
+		'#其它',
+		'#商品',
+		'#消耗品',
+		'#商品',
+		'鱼油',
+		'#任务',
+		'##任务',
+		'#珠宝',
+	}
+	JPACK_DEPOSIT = {}
+	JPACK_DRAW = {}
+end
+--@end-debug@
